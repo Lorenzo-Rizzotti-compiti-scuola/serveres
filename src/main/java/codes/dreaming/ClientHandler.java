eb0a4993fbd800c003a36767e43fab61874abb43
DreamingCodes <me@dreaming.codes>
2023-12-13 11:26:35 +0100

    Initial commit

--- src/main/java/codes/dreaming/ClientHandler.java
+++ src/main/java/codes/dreaming/ClientHandler.java
@@ -1,4 +1,47 @@
 package codes.dreaming;
 
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+
+    ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            String line;
+            while (!(line = reader.readLine()).isEmpty()) {
+                System.out.println(line);
+
+                // Analisi di stringa2 per il percorso del file
+                if (line.matches("^\\S+ \\S+ \\S+$")) {
+                    String[] parts = line.split(" ");
+                    String filePath = parts[1];
+                    if (Files.exists(Paths.get(filePath))) {
+                        System.out.println("File trovato: " + filePath);
+                    } else {
+                        System.out.println("File non trovato: " + filePath);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Errore di I/O nel BufferedReader");
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("Errore di I/O nella chiusura del Socket");
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/codes/dreaming/Main.java
+++ src/main/java/codes/dreaming/Main.java
@@ -1,7 +1,22 @@
 package codes.dreaming;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int port = 3000;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server in ascolto sulla porta " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(new ClientHandler(clientSocket)).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Errore di I/O nel ServerSocket");
+            e.printStackTrace();
+        }
     }
 }
